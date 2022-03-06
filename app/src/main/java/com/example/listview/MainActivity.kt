9ef0d9d2e534ff0eb8db21464b35df62f6eee735
Misraim <faustomisrainpoot@gmail.com>
2022-03-06 07:45:13 -0500

    onItemClickListeR Adaptador, creador y mostrar items en pantalla con leyenda

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+        var frutas:ArrayList<String> = ArrayList()
+        frutas.add("Manzana")
+        frutas.add("Durazno")
+        frutas.add("Platano")
+        frutas.add("Sandia")
+
+        val lista = findViewById<ListView>(R.id.lista)
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,frutas  )
+
+        lista.adapter= adaptador
+        lista.onItemClickListener= AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText( this, frutas.get(position), Toast.LENGTH_SHORT).show()
+
+    }  }}
